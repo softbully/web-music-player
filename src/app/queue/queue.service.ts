0d6c56d4534f1d57051da778c5a370dc44ada3ce
Hugo Leao <hugocleao@gmail.com>
2016-11-22 21:05:49 -0500

    Completed unit tests on queue service

--- src/app/queue/queue.service.ts
+++ src/app/queue/queue.service.ts
@@ -4,17 +4,18 @@ import { Display } from './display';
 import { OnPlay } from './on-play';
 import { OnStop } from './on-stop';
 import { IsPlaying } from './is-playing';
+import { Identifiable } from './identifiable';
 
 /**
  * Service to control queue list and playback;
  */
 @Injectable()
-export class QueueService<I> {
-  queue: I[] = [];
-  current: number = 0;
+export class QueueService<I extends Identifiable> {
+  queue: Identifiable[] = [];
+  currentIndex: number = 0;
 
   constructor(
-    private onPlay: OnPlay<I>,
+    private onPlay: OnPlay<Identifiable>,
     private onStop: OnStop,
     public isPlaying: IsPlaying) {
   }
@@ -22,7 +23,7 @@ export class QueueService<I> {
   /**
    * Add item to queue
    */
-  add(item: I): void {
+  add(item: Identifiable): void {
     this.queue.push(item);
 
     if (this.queue.length == 1) {
@@ -37,9 +38,8 @@ export class QueueService<I> {
   remove(index: number): void {
     this.queue.splice(index, 1);
 
-    if (this.current == index) {
-      this.current--;
-      this.stop();
+    if (this.currentIndex == index) {
+      this.currentIndex--;
       this.next();
     }
   }
@@ -55,21 +55,19 @@ export class QueueService<I> {
   /**
    * List items from queue
    */
-  list(): I[] {
+  list(): Identifiable[] {
     return this.queue;
   }
 
   next(): void {
-    if (this.current < this.queue.length - 1) {
-      this.setCurrent(this.current + 1);
-      this.play();
+    if (this.currentIndex < this.queue.length - 1) {
+      this.setCurrent(this.currentIndex + 1);
     }
   }
 
   previous(): void {
-    if (this.current > 0) {
-      this.setCurrent(this.current - 1);
-      this.play();
+    if (this.currentIndex > 0) {
+      this.setCurrent(this.currentIndex - 1);
     }
   }
 
@@ -84,29 +82,39 @@ export class QueueService<I> {
   }
 
   setCurrent(index: number): void {
-    if (this.queue.length > this.current) {
-      this.current = index;
-      this.play();
+    if (index < 0 || index >= this.queue.length) {
+      throw new Error("Cannot set current. Index '" + index +
+        "' is out of [0.." + (this.queue.length - 1) + "] bounds");
     }
 
+    this.currentIndex = index;
+
+    if (this.isPlaying()) {
+      this.play();
+    }
   }
 
-  getCurrentIndex(): number {
-    return 0;
+  getCurrentItem(): Identifiable {
+    return this.getItemAt(this.currentIndex);
   }
 
-  getCurrentItem(): I {
-    return this.queue[this.current];
+  getItemAt(index: number): Identifiable {
+    return this.queue[index];
   }
 
-  getItemAt(index: number): I {
-    return null;
-  }
-  indexOfBySongId(): number {
-    return 0;
+  indexOfBySongId(id: number): number {
+    let index = -1;
+
+    this.queue.forEach((item) => {
+      if (item.id == id) {
+        index = this.queue.indexOf(item);
+      }
+    });
+
+    return index;
   }
 
   getDisplay(): Display {
-    return null;
+    throw new Error("getDisplay() not implemented in queue service");
   }
 }
